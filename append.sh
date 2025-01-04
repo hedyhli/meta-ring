c723c3782c3f6a62cced162898a3fd4eb1c30011
~hedy <hedy.dev@protonmail.com>
2025-01-05 07:53:50 +0800

    Add dispatch event

--- append.sh
+++ append.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
-read -p "name: " name
-read -p "colophon: " colophon
-read -p "url: " url
+name="$1"
+colophon="$2"
+url="$3"
+
+if [ -z "$name" ]; then
+    read -p "name: " name
+    read -p "colophon: " colophon
+    read -p "url: " url
+fi
 
 awk -v name="$name" -v colophon="$colophon" -v url="$url" \
     -f append.awk \
